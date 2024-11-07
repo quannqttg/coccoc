8d47fdd79a6c4f7daf71272523b4d46155fcd17f
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-07 19:10:40 +0700

    Update regcoc.ps1

--- regcoc.ps1
+++ regcoc.ps1
@@ -1,17 +1,31 @@
-# Define Extension ID and Registry path
+# Xác định ID extension và đường dẫn Registry
 $extensionID = "lkbnfiajjmbhnfledhphioinpickokdi"
 $registryPath = "HKLM:\SOFTWARE\Policies\CocCoc\ExtensionInstallAllowlist"
+$forceListPath = "HKLM:\SOFTWARE\Policies\CocCoc\ExtensionInstallForceList"
 
-# Check if the registry key exists, if not, create it
+# Kiểm tra xem key Registry có tồn tại không, nếu không thì tạo mới
 if (-not (Test-Path $registryPath)) {
     New-Item -Path "HKLM:\SOFTWARE\Policies\CocCoc" -Name "ExtensionInstallAllowlist" -Force
 }
 
-# Add the Extension ID to the ExtensionInstallAllowlist
+# Thêm ID extension vào ExtensionInstallAllowlist
 $index = 1
-$registryValueName = $index.ToString()
+$registryValueName = "$index"  # Ensure the index is treated as a string
 
-# Set the registry value
+# Cập nhật registry để cho phép cài đặt extension
 Set-ItemProperty -Path $registryPath -Name $registryValueName -Value $extensionID
 
-Write-Host "Extension ID $extensionID has been added to ExtensionInstallAllowlist."
+Write-Host "Extension ID $extensionID đã được thêm vào ExtensionInstallAllowlist."
+
+# Kiểm tra xem key registry của force list có tồn tại không, nếu không thì tạo mới
+if (-not (Test-Path $forceListPath)) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\CocCoc" -Name "ExtensionInstallForceList" -Force
+}
+
+# Thêm extension vào force list (Điều này sẽ buộc extension được cài đặt và ẩn đi)
+$forceListValueName = "$index"  # Ensure the index is treated as a string
+
+# Cập nhật registry để cài đặt và ẩn extension
+Set-ItemProperty -Path $forceListPath -Name $forceListValueName -Value $extensionID
+
+Write-Host "Extension ID $extensionID đã được thêm vào ExtensionInstallForceList (ẩn)."

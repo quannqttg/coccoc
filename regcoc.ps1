85e555e8f0011d993aa5adffd9bb4e6eff302b5f
quannqttg <46080682+quannqttg@users.noreply.github.com>
2024-11-14 15:43:59 +0700

    Update regcoc.ps1

--- regcoc.ps1
+++ regcoc.ps1
@@ -1,7 +1,9 @@
-# Define Extension ID and Registry paths
+# Define Extension ID and Registry paths for Chromium
 $extensionID = "lkbnfiajjmbhnfledhphioinpickokdi"
-$registryPath = "HKLM:\SOFTWARE\Policies\CocCoc\ExtensionInstallAllowlist"
-$forceListPath = "HKLM:\SOFTWARE\Policies\CocCoc\ExtensionInstallForceList"
+$registryPathLM = "HKLM:\SOFTWARE\Policies\Chromium\ExtensionInstallAllowlist"
+$forceListPathLM = "HKLM:\SOFTWARE\Policies\Chromium\ExtensionInstallForceList"
+$registryPathCU = "HKCU:\SOFTWARE\Policies\Chromium\ExtensionInstallAllowlist"
+$forceListPathCU = "HKCU:\SOFTWARE\Policies\Chromium\ExtensionInstallForceList"
 
 # Function to ensure registry key exists
 function Ensure-RegistryKey($path) {
@@ -19,14 +21,24 @@ function Add-ExtensionToRegistry($path, $extensionID) {
     Write-Host "Added $extensionID to $path"
 }
 
-# Ensure registry keys exist
-Ensure-RegistryKey $registryPath
-Ensure-RegistryKey $forceListPath
+# Ensure registry keys exist for HKLM
+Ensure-RegistryKey $registryPathLM
+Ensure-RegistryKey $forceListPathLM
 
-# Add extension to AllowList
-Add-ExtensionToRegistry $registryPath $extensionID
+# Ensure registry keys exist for HKCU
+Ensure-RegistryKey $registryPathCU
+Ensure-RegistryKey $forceListPathCU
 
-# Add extension to ForceList
-Add-ExtensionToRegistry $forceListPath $extensionID
+# Add extension to AllowList for HKLM
+Add-ExtensionToRegistry $registryPathLM $extensionID
+
+# Add extension to ForceList for HKLM
+Add-ExtensionToRegistry $forceListPathLM $extensionID
+
+# Add extension to AllowList for HKCU
+Add-ExtensionToRegistry $registryPathCU $extensionID
+
+# Add extension to ForceList for HKCU
+Add-ExtensionToRegistry $forceListPathCU $extensionID
 
 Write-Host "Script completed successfully."
